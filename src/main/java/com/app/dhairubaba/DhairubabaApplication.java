373bbdb8d7d2c85983d8ee398e13683d805182a6
rambabumaina <ram.b.maina01@gmail.com>
2021-12-27 08:38:25 +0530

    first:configurations

---
+++ src/main/java/com/app/dhairubaba/DhairubabaApplication.java
@@ -0,0 +1,13 @@
+package com.app.dhairubaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhairubabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhairubabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/dhairubaba/DhairubabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dhairubaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhairubabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
